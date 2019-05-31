bb29a4406806881b9cbb82e2c4684d40ad4c998a
Brazen <764146802@qq.com>
2019-05-31 22:06:18 +0800

    添加icon和iconPositon

--- src/button.vue
+++ src/button.vue
@@ -1,15 +1,27 @@
 <template>
   <div class="button-wrapper">
-    <button class="z-button">按钮</button>
+    <button class="z-button"  :class="{[`icon-${iconPosition}`]:true}">
+      <svg class="icon" v-if="icon">
+        <use :xlink:href="`#i-${icon}`"></use>
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
   </div>
 </template>
 
 <script>
   export default {
     name:'ButtonWheel',
-    data(){
-      return {
-
+    props:{
+      icon:{},
+      iconPosition:{
+        type:String,
+        default:'left',
+        validator(value){
+          return value === 'left' || value === 'right'
+        }
       }
     }
   }
@@ -22,20 +34,30 @@
   $border-color-hover:#666;
   $border-color:#999;
   $bg-active-color:#666 ;
+  
   .z-button{  
     background: $background-color;
     border-radius: $border-radius;
     padding: 0 1em;
     height: $button-height;
     border: 1px solid $border-color;
-    &:focus{
-      outline: none;
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    vertical-align: top;
+    &.icon-left{
+      > .icon{ order:1 ;margin-right: .2em;}
+      > .content{ order:2; }
     }
-    &:active{
-      background: $bg-active-color;
+    &.icon-right{
+      > .icon{ order:2 ;margin-right: 0;margin-left: .2em;}
+      > .content{ order:1 ;}
     }
-    &:hover{
-      border-color:$border-color-hover;
-    }
+    &:focus{ outline: none; }
+    &:active{ background: $bg-active-color; }
+    &:hover{ border-color:$border-color-hover; }
+  }
+  .button-wrapper{
+    display: inline-block;
   }
 </style>
