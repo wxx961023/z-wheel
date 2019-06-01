ec913acd2fe2d795d288c08089a5059f21d01449
Brazen <764146802@qq.com>
2019-06-01 17:17:54 +0800

    .gitignore is now working

---
+++ src/button-group.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'ButtonGroupWheel',
+  }
+</script>
+
+<style lang='scss' scoped>
+  .button-group{
+    display:inline-flex;
+    > .z-button{
+      border-radius:0;
+      margin-left:-1px;
+      &:first-child{
+        border-top-left-radius: 4px;
+        border-bottom-left-radius: 4px;
+      }
+      &:last-child{
+        border-top-right-radius: 4px;
+        border-bottom-right-radius: 4px;
+      }
+      &:hover{
+        z-index: 1;
+      }
+    }
+  }
+</style>
