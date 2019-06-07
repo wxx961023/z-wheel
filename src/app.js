3fe907305f5fd6f089af9ded1460be912ec63f7b
Brazen <764146802@qq.com>
2019-06-08 00:11:34 +0800

    row和col的基本样式

--- src/app.js
+++ src/app.js
@@ -3,12 +3,16 @@ import Button from './button/button.vue';
 import Icon from './button/icon.vue';
 import ButtonGroup from './button/button-group.vue';
 import Input from './input/input.vue';
+import Row from './grid/row.vue'
+import Col from './grid/col.vue'
+
 
 Vue.component('z-button',Button)
 Vue.component('z-icon',Icon)
 Vue.component('z-button-group',ButtonGroup)
 Vue.component('z-input',Input)
-
+Vue.component('z-row',Row)
+Vue.component('z-col',Col)
 
 new Vue({
   el:'#app',
