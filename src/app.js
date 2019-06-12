6fb17fbf0fd66733ab061ab4e2b04053fd09395c
Brazen <764146802@qq.com>
2019-06-12 14:00:19 +0800

    toast和plugin

--- src/app.js
+++ src/app.js
@@ -3,27 +3,30 @@ import Button from './button/button.vue';
 import Icon from './button/icon.vue';
 import ButtonGroup from './button/button-group.vue';
 import Input from './input/input.vue';
-import Row from './grid/row.vue'
-import Col from './grid/col.vue'
-import Layout from './layout/layout.vue'
-import Header from './layout/header.vue'
-import Content from './layout/content.vue'
-import Footer from './layout/footer.vue'
-import Sider from './layout/sider.vue'
+import Row from './grid/row.vue';
+import Col from './grid/col.vue';
+import Layout from './layout/layout.vue';
+import Header from './layout/header.vue';
+import Content from './layout/content.vue';
+import Footer from './layout/footer.vue';
+import Sider from './layout/sider.vue';
+import Toast from './toast/toast.vue';
+import Plugin from './toast/plugin.js';
 
 
-
-Vue.component('z-button',Button)
-Vue.component('z-icon',Icon)
-Vue.component('z-button-group',ButtonGroup)
-Vue.component('z-input',Input)
-Vue.component('z-row',Row)
-Vue.component('z-col',Col)
-Vue.component('z-layout',Layout)
-Vue.component('z-header',Header)
-Vue.component('z-content',Content)
-Vue.component('z-footer',Footer)
-Vue.component('z-sider',Sider)
+Vue.component('z-button',Button);
+Vue.component('z-icon',Icon);
+Vue.component('z-button-group',ButtonGroup);
+Vue.component('z-input',Input);
+Vue.component('z-row',Row);
+Vue.component('z-col',Col);
+Vue.component('z-layout',Layout);
+Vue.component('z-header',Header);
+Vue.component('z-content',Content);
+Vue.component('z-footer',Footer);
+Vue.component('z-sider',Sider);
+Vue.component('z-toast',Toast);
+Vue.use(Plugin)
 
 new Vue({
   el:'#app',
@@ -31,5 +34,13 @@ new Vue({
     loading1:false,
     loading2:false,
     message:'hi'
+  },
+  created(){
+    
+  },
+  methods:{
+    showToast(){
+      this.$toast()
+    }
   }
 })
---
+++ src/toast/toast.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'ToastWheel',
+    data(){
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style lang='scss' scoped>
+  .toast{
+
+  }
+</style>
