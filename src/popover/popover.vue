a116cbc81a4eb6395d1465bd643ef68abcf50fcc
Brazen <764146802@qq.com>
2019-06-28 22:07:58 +0800

    解决overflow：hidden的bug

--- src/popover/popover.vue
+++ src/popover/popover.vue
@@ -1,9 +1,11 @@
 <template>
-    <div class="popover" @click="xxx">
-        <div class="content-wrapper" v-if="visible">
+    <div class="popover" @click.stop="xxx">
+        <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
             <slot name="content" class="name"></slot>
         </div>
-        <slot></slot>
+        <span ref="triggerWrapper">
+            <slot></slot>
+        </span>
     </div>
 </template>
 
@@ -18,6 +20,19 @@
         methods:{
             xxx(){
                 this.visible = !this.visible
+                if(this.visible){
+                    this.$nextTick(()=>{
+                        document.body.appendChild(this.$refs.contentWrapper)
+                        let {width,height,left,top} = this.$refs.triggerWrapper.getBoundingClientRect()
+                        this.$refs.contentWrapper.style.left = window.scrollX + left + 'px'
+                        this.$refs.contentWrapper.style.top = window.scrollY + top +'px'
+                        let eventHandler = ()=>{
+                            this.visible = false
+                            document.removeEventListener('click',eventHandler)
+                        }
+                        document.addEventListener('click',eventHandler)
+                    })
+                }
             }
         }
     }
@@ -28,12 +43,11 @@
         display: inline-block;
         vertical-align: top;
         position: relative;
-        > .content-wrapper{
-            border: 1px solid pink;
-            position: absolute;
-            bottom: 100%;
-            left: 0;
-            box-shadow: 0 0 3px rgba(0,0,0,0.5)
-        }
+    }
+    .content-wrapper{
+        border: 1px solid pink;
+        position: absolute;
+        transform: translateY(-100%);
+        box-shadow: 0 0 3px rgba(0,0,0,0.5)
     }
 </style>
